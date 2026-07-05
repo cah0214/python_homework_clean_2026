b48214dc18b00e83e631a7005e42af97bfd368ab
Crystal Hoefener <crystal061410@gmail.com>
2026-07-05 15:39:50 -0400

    Complete Assignment 1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,1 +1,32 @@
-# Write your code here.
+def hello():
+    return "Hello!"
+
+
+def greet(name):
+    return f"Hello, {name}!"
+
+
+def calc(num1, num2, operation="multiply"):
+    pass
+
+
+def data_type_conversion(value, data_type):
+    pass
+
+
+def grade(*scores):
+    pass
+
+
+def repeat(string, count):
+    pass
+
+
+def student_scores(score_type, **kwargs):
+    pass
+def titleize(string):
+    pass
+def hangman(word, guesses):
+    pass
+def pig_latin(string):
+    pass
