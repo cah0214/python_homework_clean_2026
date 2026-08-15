466d6529bbca8ca8cb9318cb8890e280dd016cf1
Crystal Hoefener <crystal061410@gmail.com>
2026-08-15 19:19:40 -0400

    Fix diary file handling

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -1,3 +1,4 @@
+from email import message
 import traceback
 
 # Task 1
@@ -5,27 +6,27 @@ try:
     with open("diary.txt", "a") as diary_file:
         first_entry = True
 
-    while True:
-        if first_entry:
+        while True:
+            if first_entry:
                 entry = input("What happened today? ")
                 first_entry = False
-        else:
-            entry = input("What else? ")
+            else:
+                entry = input("What else? ")
 
-        diary_file.write(entry + "\n")
+            diary_file.write(entry + "\n")
 
-        if entry =="done for now":
+            if entry =="done for now":
                 break
 except Exception as e:
-            trace_back = traceback.extract_tb(e.__traceback__)
-            stack_trace = list()
-            for trace in trace_back:
-                stack_trace.append(f'File : {trace[0]}, Func.Name : {trace[2]}, Line No : {trace[1]}, Message : {trace[3]}')
+    print("An exception occured.")
+    print(f"Exception type: {type(e).__name__}")
 
-            print(f"Exception type: {type(e).__name__}")
-            message = str(e)
+    trace_back = traceback.extract_tb(e.__traceback__)
+    stack_trace = list()
+    for trace in trace_back:
+        stack_trace.append(f'File : {trace[0]}, Func.Name : {trace[2]}, Line No : {trace[1]}, Message : {trace[3]}')
+            
+    if message:
+        print(f"Exception message : {message}")
 
-            if message:
-                print(f"Exception message : {message}")
-
-            print(f"Stack trace: {stack_trace}")
+        print(f"Stack trace: {stack_trace}")
